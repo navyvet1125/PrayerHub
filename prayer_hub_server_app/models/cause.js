2fee696e7f12a5026f1fca8b1ea6924d55e9ab3b
Evan Washington <enavy04@gmail.com>
2016-05-22 12:39:33 -0700

    updated cause model

--- prayer_hub_server_app/models/cause.js
+++ prayer_hub_server_app/models/cause.js
@@ -1,7 +1,9 @@
 var mongoose = require('mongoose');
+var User = require('./user');
 var causeSchema = new mongoose.Schema({
 	title: String,
 	body: String,
+	creator: {type: mongoose.Schema.Types.ObjectId, ref: 'User'},
 	category: String,
 	createdAt: Date,
 	expiration: Date
