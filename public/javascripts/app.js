971cb7b4dd148cfd023c10dd999cd917007bc5fb
Evan Washington <enavy04@gmail.com>
2016-05-23 22:30:56 -0700

    added angular functionality

---
+++ public/javascripts/app.js
@@ -0,0 +1,3 @@
+// Define a new module.  The first argument will be the name of the app.  The second argument is an
+// array of dependencies.  The empty array is necessary.  It is for dependencies.
+angular.module('prayerHubApp', []);
